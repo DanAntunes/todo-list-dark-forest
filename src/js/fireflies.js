ab40f453005ca790ed0a1d0964a6016ea407b0a8
Soren <danilo-258@hotmail.com>
2025-03-16 01:34:10 -0300

    refactor(scripts): move fireflies.js  to src/assets/js

--- src/js/fireflies.js
+++
@@ -1,7 +0,0 @@
-const quantity = 15;
-const container = document.getElementById("fireflies-container");
-for (let i = 0; i < quantity; i++) {
-  const div = document.createElement("div");
-  div.className = "firefly";
-  container.appendChild(div);
-}
